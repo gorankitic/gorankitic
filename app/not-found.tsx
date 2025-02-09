09f1848b92fe53e35e9220176250b6cbc69f2237
Goran Kitic <kiticgoran90@gmail.com>
2025-02-09 22:55:02 +0100

    not-found update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,7 @@ const NotFound = () => {
     return (
         <main className="flex flex-col justify-center flex-1 text-center">
             <h2 className="text-3xl">There was a problem.</h2>
-            <p className="mt-6">We couldn't find that page.</p>
+            <p className="mt-6">We couldn&apos;t find that page.</p>
             <p>Go back to the <Link href='/' className="text-teal-600 underline">Homepage</Link>.</p>
         </main>
     )
